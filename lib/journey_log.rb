4ec0654ea4273f59ec156bd4fd78c14165209d74
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-11-08 21:26:26 +0000

    added start method for journey_log

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,7 +1,17 @@
+require_relative "./journey.rb"
 class JourneyLog
   attr_reader :journey_history, :current_journey
   def initialize
     @journey_history = []
     @current_journey = nil
+  end
+  def start(station)
+    get_current_journey
+    @current_journey.entry_station = station.name
+    @journey_history << @current_journey
+    true
+  end
+  def get_current_journey
+    @current_journey ||= Journey.new
   end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,8 +1,14 @@
 require 'journey_log'
+require 'journey.rb'
+
 describe JourneyLog do
   let(:journey_log) {JourneyLog.new}
-  it "creates an instance of JourneyLog" do
-    expect(journey_log.instance_of? JourneyLog).to eq true
+  let(:journey_1) {double :journey_1}
+  let(:station_1) {double :station_1}
+  let(:station_2) {double :station_2}
+  before(:each) do
+    allow(station_1).to receive(:name).and_return("Euston")
+    allow(station_2).to receive(:name).and_return("")
   end
   it "initializes journey_history to empty array" do
     expect(journey_log.journey_history).to eq []
@@ -10,4 +16,20 @@ describe JourneyLog do
   it "initializes current_journey to nil" do
     expect(journey_log.current_journey).to eq nil
   end
+  it "stores incomplete journeys when we don't touch out" do
+    entry_station_only = []
+    journey_log.start(station_1)
+    entry_station_only << journey_log.journey_history[0].entry_station
+    entry_station_only << journey_log.journey_history[0].exit_station
+    expect(entry_station_only).to eq ["Euston", ""]
+  end
+  it "get_current_journey returns the journey when touched in" do
+    journey_log.start(station_1)
+    current_journey = journey_log.current_journey
+    expect(journey_log.get_current_journey).to eq current_journey
+  end
+  it "start returns true" do
+    expect(journey_log.start(station_1)).to eq true
+  end
+
 end
